f6884b2c594430009df2561dc5a387d62e4fc774
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-06-03 17:49:46 -0300

    ADD Layout Home controller index e builder services

--- ListaDeCompras.WebApplication/Program.cs
+++ ListaDeCompras.WebApplication/Program.cs
@@ -1,6 +1,14 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorOptions((options) =>
+{
+    options.ViewLocationFormats.Clear();
+
+    options.ViewLocationFormats.Add("/Modulo{1}/Apresentacao/Views/{0}.cshtml");
+
+    options.ViewLocationFormats.Add("/Compartilhado/Apresentacao/Views/{0}.cshtml");
+});
 
 var app = builder.Build();
 
